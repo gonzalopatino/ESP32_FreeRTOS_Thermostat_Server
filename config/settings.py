47b15b19cb1bb2e123df9010a1ee7b369adda180
Gonzalo <gpatinoc92@gmail.com>
2025-11-20 20:05:56 -0800

    POSTGRESQL migration succesful

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,9 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "thermostat",
+        "USER": "postgres",
+        "PASSWORD": "21217754Go92@",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
